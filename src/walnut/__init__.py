06830353acc8c49db59617d078c5a448ab4cd7dc
Esteban Del Boca <esteban.delboca-ext@bluecore.com>
2022-06-03 14:05:55 -0300

    promoting version

--- src/walnut/__init__.py
+++ src/walnut/__init__.py
@@ -12,4 +12,4 @@ from walnut.steps.core import LoadSettingsStep as LoadSettingsStep
 from walnut.steps.core import Base64DecodeStep as Base64DecodeStep
 
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
