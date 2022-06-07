75ced777b4c5ec402870ed7fbd10e31ce9c47817
Esteban Del Boca <esteban.delboca-ext@bluecore.com>
2022-06-07 15:29:45 -0300

    GetPodsStep

--- src/walnut/__init__.py
+++ src/walnut/__init__.py
@@ -12,4 +12,4 @@ from walnut.steps.core import LoadSettingsStep as LoadSettingsStep
 from walnut.steps.core import Base64DecodeStep as Base64DecodeStep
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
--- src/walnut/steps/kubernetes.py
+++ src/walnut/steps/kubernetes.py
@@ -1,11 +1,12 @@
 from typing import Sequence
+import pendulum
 from walnut import Step
 from walnut.errors import StepExcecutionError
 
 
 class KubernetesStep(Step):
     """
-    Base and Abstract Step to interact with Kubernetes. All Steps that require 
+    Base and Abstract Step to interact with Kubernetes. All Steps that require
     Kubernetes access should inherit from this class.
     """
 
@@ -19,15 +20,18 @@ class KubernetesStep(Step):
     def get_client(self):
         try:
             from kubernetes import client, config
+
             config.load_kube_config(context=self.context)
             return client.CoreV1Api()
         except ImportError:
-            raise StepExcecutionError("kubernetes client is required: pip install kubernetes")
+            raise StepExcecutionError(
+                "kubernetes client is required: pip install kubernetes"
+            )
         except Exception as err:
             raise StepExcecutionError(f"kubernetes client error: {err}")
 
     def execute(self, params: dict) -> dict:
-        return NotImplemented("KubernetesStep should not be called directly")
+        return super().execute(params)
 
 
 class GetSecretStep(KubernetesStep):
@@ -43,15 +47,60 @@ class GetSecretStep(KubernetesStep):
         self.name = name
 
     def execute(self, params: dict) -> dict:
+        super().execute(params)
         try:
             client = self.get_client()
             s = client.read_namespaced_secret(self.name, namespace=self.namespace)
-            return {
-                "kubernetes": {
-                    "secrets": {
-                        self.name: s.to_dict()
+            return {"kubernetes": {"secrets": {self.name: s.to_dict()}}}
+        except Exception as err:
+            raise StepExcecutionError(f"kubernetes client error: {err}")
+
+
+class GetPodsStep(KubernetesStep):
+    """
+    Returns a dictionary containing the pods with the "get pods" information:
+    NAME                             READY   STATUS      RESTARTS   AGE
+    {pod_name}                       1/1     Running     0          24h
+
+    Using the following format:
+    {
+        "kubernetes": {
+            {namespace}: {
+                "pods": {
+                    {pod_name}: {
+                        "name": {pod_name},
+                        "ready": [{ready}, {count}]
+                        "status": {status},
+                        "restarts": {restart_count},
+                        "age": {age_in_minutes}
                     }
                 }
             }
+        }
+    }
+    """
+
+    def execute(self, params: dict) -> dict:
+        super().execute(params)
+        try:
+            client = self.get_client()
+            pods = client.list_namespaced_pod(namespace=self.namespace)
+            items = {}
+            for p in pods.items:
+                restarts = 0
+                ready = 0
+                for cs in p.status.container_statuses:
+                    restarts += cs.restart_count
+                    ready = ready + 1 if cs.ready else ready
+                st = pendulum.now() - pendulum.instance(p.status.start_time)
+                item = {
+                    "name": p.metadata.name,
+                    "ready": [ready, len(p.status.container_statuses)],
+                    "status": p.status.phase,
+                    "restarts": restarts,
+                    "age": st.minutes,
+                }
+                items[p.metadata.name] = item
+            return {"kubernetes": {self.namespace: {"pods": items}}}
         except Exception as err:
             raise StepExcecutionError(f"kubernetes client error: {err}")
